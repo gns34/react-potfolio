855a5191e69d1d05e647a8e1433802ddd831303e
Gopi Nath <gnsgns199@gmail.com>
2024-12-12 22:16:22 +0530

    Card Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
+import {Profile} from "./Gallery.jsx";
 
-import './App.css'
-const person = {
-    name: 'Gregorio Y. Zara',
-    theme: {
-        backgroundColor: 'green',
-        color: 'cyan',
-        borderRadius:'15px'
-    }
-};
-export default function App() {
-    return (
-        <div style={person.theme}>
-            <h1>{person.name}&#39;s Todos</h1>
-            <img
-                className="avatar"
-                src="https://i.imgur.com/7vQD0fPs.jpg"
-                alt="Gregorio Y. Zara"
 
-            />
-            <ul>
-                <li>Improve the videophone</li>
-                <li>Prepare aeronautics lectures</li>
-                <li>Work on the alcohol-fuelled engine</li>
-            </ul>
-        </div>
+export default function App(){
+    return(
+        <Profile/>
     );
 }
-
---
+++ src/Gallery.jsx
@@ -0,0 +1,22 @@
+import Avatar from "./Avatar.jsx";
+
+function Card({ children }) {
+    return (
+        <div className="card">
+            {children}
+        </div>
+    );
+}
+export function Profile() {
+    return (
+        <Card>
+            <Avatar
+                size={100}
+                person={{
+                    name: 'Katsuko Saruhashi',
+                    imageId: 'YfeOqp2'
+                }}
+            />
+        </Card>
+    );
+}
